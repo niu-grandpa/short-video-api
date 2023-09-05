00a77aae3ba0074cb2c613022a6e18ff2f9c6a7c
niu-grandpa <2864103063@qq.com>
2023-09-05 21:09:17 +0800

    增加上传接口

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import Paths from '../constants/Paths';
 
-import ActionRoutes from '../ActionRoutes';
-import CommentRoutes from '../CommentRoutes';
-import PermissionRoutes from '../PermissionRoutes';
+import actionRoutes from '../ActionRoutes';
+import commentRoutes from '../CommentRoutes';
+import permissionRoutes from '../PermissionRoutes';
+import uploadRoutes from '../UploadRoutes';
 import userRouter from '../UserRoutes';
 import videoRouter from '../VideoRoutes';
 
@@ -12,8 +13,9 @@ const apiRouter = Router();
 apiRouter
   .use(Paths.Users.Base, userRouter)
   .use(Paths.Videos.Base, videoRouter)
-  .use(Paths.Actions.Base, ActionRoutes)
-  .use(Paths.Comments.Base, CommentRoutes)
-  .use(Paths.Permissions.Base, PermissionRoutes);
+  .use(Paths.Actions.Base, actionRoutes)
+  .use(Paths.Comments.Base, commentRoutes)
+  .use(Paths.Upload.Base, uploadRoutes)
+  .use(Paths.Permissions.Base, permissionRoutes);
 
 export default apiRouter;
